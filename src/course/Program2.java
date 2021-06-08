21a83b9588b9f4523bf1e6c446b4876ec5dcbeb9
Yuri Couto <yuricolt@gmail.com>
2021-06-08 10:13:05 -0300

    Projeto Pronto

---
+++ src/course/Program2.java
@@ -0,0 +1,37 @@
+package course;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Program2 {
+	
+	public static void main(String[] args) throws ParseException {
+		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+		Date d = Date.from(Instant.parse("2021-06-21T15:43:20Z"));
+		
+	
+		System.out.println("Horario do UTC");
+	
+		System.out.println("d: " + sdf.format(d));
+	
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		
+		System.out.println(sdf.format(d));
+
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = cal.get(Calendar.MONTH);
+		
+		System.out.println(minutes);
+		System.out.println(month + 1);
+		
+	}
+}
